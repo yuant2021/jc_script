47f9d915aaf802846298cb0692327927baab1832
YuanTianhang <42244015+yuant2007@users.noreply.github.com>
2022-04-04 13:59:58 +0800

    Update XrayR.sh

--- XrayR.sh
+++ XrayR.sh
@@ -10,8 +10,8 @@ xrayr_install(){
 xrayr_config(){
     wget https://github.com/yuant2007/jc_script/raw/main/XrayR_template.yml -O /etc/XrayR/config.yml
     sed -i "s/{{!PanelType!}}/$a/g" /etc/XrayR/config.yml
-    sed -i "s/{{!ApiHost!}}/$b/g" /etc/XrayR/config.yml
-    sed -i "s#{{!ApiKey!}}#$c#g" /etc/XrayR/config.yml
+    sed -i "s#{{!ApiHost!}}#$b#g" /etc/XrayR/config.yml
+    sed -i "s/{{!ApiKey!}}/$c/g" /etc/XrayR/config.yml
     sed -i "s/{{!NodeType!}}/$d/g" /etc/XrayR/config.yml
     echo -n '请输入节点ID:'&&read id
     sed -i "s/{{!NodeID!}}/$id/g" /etc/XrayR/config.yml
