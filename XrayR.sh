abba7600e84e82c181c2bf07697c53593bd243bc
YuanTianhang <42244015+yuant2007@users.noreply.github.com>
2022-04-04 13:33:57 +0800

    Create XrayR.sh

---
+++ XrayR.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+xrayr_install(){
+    bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
+}
+
+xrayr_config(){
+    wget https://github.com/yuant2007/jc_script/raw/main/XrayR_template.yml -O /etc/XrayR/config.yml
+    sed -i 's/{{!PanelType!}}/$1/g' /etc/XrayR/config.yml
+    sed -i 's/{{!ApiHost!}}/$2/g' /etc/XrayR/config.yml
+    sed -i 's/{{!ApiKey!}}/$3/g' /etc/XrayR/config.yml
+    sed -i "s/{{!NodeType!}}/$4/g" /etc/XrayR/config.yml
+    echo -n '请输入节点ID:'&&read id
+    sed -i "s/{{!NodeID!}}/$id/g" /etc/XrayR/config.yml
+    XrayR restart
+}
+
+xrayr_config
