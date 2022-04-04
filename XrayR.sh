23128d8e5c447f84c0d96a309cf8d6663e9027ab
YuanTianhang <42244015+yuant2007@users.noreply.github.com>
2022-04-04 13:46:10 +0800

    Update XrayR.sh

--- XrayR.sh
+++ XrayR.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-
+a=$1
+b=$2
+c=$3
+d=$4
 xrayr_install(){
     bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
 }
 
 xrayr_config(){
-    echo $1
     wget https://github.com/yuant2007/jc_script/raw/main/XrayR_template.yml -O /etc/XrayR/config.yml
-    sed -i "s/{{!PanelType!}}/$1/g" /etc/XrayR/config.yml
-    sed -i "s/{{!ApiHost!}}/$2/g" /etc/XrayR/config.yml
-    sed -i "s/{{!ApiKey!}}/$3/g" /etc/XrayR/config.yml
-    sed -i "s/{{!NodeType!}}/$4/g" /etc/XrayR/config.yml
+    sed -i "s/{{!PanelType!}}/$a/g" /etc/XrayR/config.yml
+    sed -i "s/{{!ApiHost!}}/$b/g" /etc/XrayR/config.yml
+    sed -i "s/{{!ApiKey!}}/$c/g" /etc/XrayR/config.yml
+    sed -i "s/{{!NodeType!}}/$d/g" /etc/XrayR/config.yml
     echo -n '请输入节点ID:'&&read id
     sed -i "s/{{!NodeID!}}/$id/g" /etc/XrayR/config.yml
     XrayR restart
