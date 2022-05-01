fd598c4a8e8140553c89e849701d9cac7843486a
YuanTianhang <42244015+yuant2021@users.noreply.github.com>
2022-05-01 23:34:56 +0800

    Update XrayR.sh

--- XrayR.sh
+++ XrayR.sh
@@ -4,11 +4,11 @@ b=$2
 c=$3
 d=$4
 xrayr_install(){
-    bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
+    wget -N https://raw.githubusercontents.com/Misaka-blog/XrayR-script/master/install.sh && bash install.sh
 }
 
 xrayr_config(){
-    wget https://github.com/yuant2007/jc_script/raw/main/XrayR_template.yml -O /etc/XrayR/config.yml
+    wget https://github.com/yuant2021/jc_script/raw/main/XrayR_template.yml -O /etc/XrayR/config.yml
     sed -i "s/{{!PanelType!}}/$a/g" /etc/XrayR/config.yml
     sed -i "s#{{!ApiHost!}}#$b#g" /etc/XrayR/config.yml
     sed -i "s/{{!ApiKey!}}/$c/g" /etc/XrayR/config.yml
